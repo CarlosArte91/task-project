216bdfecac5a83edf554040c9db2f3c2350e60cc
Carlos Cruz <carturo99@gmail.com>
2022-12-15 14:25:08 -0500

    tarea de contacto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import TaskList from './components/container/task_list';
+import ContactList from './components/container/contact_list';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <TaskList />
+        <ContactList />
       </header>
     </div>
   );
